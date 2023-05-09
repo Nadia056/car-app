6847eb2c24170ecfc64b5c96d4f6da8eb296adc1
nadia356 <110473332+nadia356@users.noreply.github.com>
2023-05-09 02:06:05 -0600

    NS

--- app/Http/Controllers/BikeController.php
+++ app/Http/Controllers/BikeController.php
@@ -2,65 +2,74 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Bike;
+use App\Models\Client;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use tidy;
 
-class BikeController extends Controller
+class AuthController extends Controller
 {
-    public function show()
+    public function login(Request $request)
     {
-        $bike = Bike::all();
-        return response()->json($bike);
-
-    }
-    public function showone($id)
-    {
-        $bike = Bike::find($id);
-        if (!$bike) {
-            return response()->json('not found');
+        $user = Client::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'error' => 'Invalid credentials'
+            ],400);
         }
-        return response()->json(['bike'=>$bike]);
 
-    }
-
-    public function store (Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'brand' => 'required|string|max:255',
-            'model' => 'required|string|max:255',
-            'year' => 'required|integer',
-            'color' => 'required|string|max:255',
-        ]);
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 422);
+        if ($user->active==0) {
+            return response()-> json([
+                'error' => 'Please activate your account'
+            ],400);
+          
         }
-        $bike = Bike::create([
-            'brand' => $request->brand,
-            'model' => $request->model,
-            'year' => $request->year,
-            'color' => $request->color,
-        ]);
-        $bike->save();
+        $token = $user->createToken('auth_token')->plainTextToken;
         return response()->json([
-            'message' => 'Successfully created bike!'
-        ], 201);
+            'token' => $token,
+        ],200
+);
+       
+    }
+    public function logout(Request $request)
 
-    }
-    public function update(Request $request, $id)
     {
-        $bike = Bike::find($id);
-        $bike->update($request->all());
-        return response()->json($bike);
-    }
-    public function delete(Request $request, $id)
-    {
-        $bike = Bike::find($id);
-        if (!$bike) {
-            return response()->json('not found');
+       
+        if (Auth::check()) {
+            $request->user()->currentAccessToken()->delete();
         }
-        $bike->delete($request->all());
-        return response()->json('deleted');
+        return response()->json(['message' => 'Successfully logged out']);
     }
 
+    
+    public function active(Request $request, $id)
+    {
+        $client = Client::find($id);
+        if (!$client) {
+            return response()->json([
+                'error' => 'Invalid credentials'
+            ],400);
+        }
+        if ($request->activation_code !== $client->activation_code) {
+            return response()->json([
+                'error' => 'Invalid activation code'
+            ],400);
+        }
+    
+        $client->active = 1;
+        $client->save();
+        return response()->json([
+            'message' => 'Successfully activated user!',
+            'user' => $client
+        ],200);
+        
+    }
+
+    public function role(Request $request)
+    {
+        $user = $request->user();
+        return response()->json($user->role);
+    }
+    
 }
